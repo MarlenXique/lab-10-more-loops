1d4d75ee2e3002adfddec5e2bc9acbf07cafb65b
Marlen Xique <marlenxique@Marlens-MBP.lan>
2020-11-19 17:13:03 -0500

    hanoi

---
+++ hanoi.py
@@ -0,0 +1,17 @@
+def move(f,t):
+    print("move from {} to {}".format(f,t))
+move("A","C")
+def moveVia(f,v,t):
+    move(f,t)
+    move(v,t)
+moveVia("A","B","C")
+def foo(x):
+    foo(x)
+def hanoi(n,f,h,t):
+    if n==0:
+        pass
+    else:
+        hanoi(n-1,f,t,h)
+        move(f,t)
+        hanoi(n-1,h,f,t)
+hanoi(4,"A","B","C")
