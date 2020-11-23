8e9436aa2770dda537e667353c568c831641981b
Marlen Xique <marlenxique@Marlens-MBP.lan>
2020-11-23 15:34:56 -0500

    hanoi

--- hanoi.py
+++ hanoi.py
@@ -1,17 +1,22 @@
-def move(f,t):
-    print("move from {} to {}".format(f,t))
+def move(f,t): #from,to.A value passed to a function (or method) when calling the function.
+    print("move from {} to {}".format(f,t)) #.format is amethod because of the dot. ots a strong, class of data.
 move("A","C")
-def moveVia(f,v,t):
-    move(f,t)
-    move(v,t)
-moveVia("A","B","C")
-def foo(x):
-    foo(x)
+def moveVia(f,v,t): #from,via,to.
+    move(f,v) #function call to move(): move from step1 to step 2 (out of 3)
+        #at first the destinations is v
+    move(v,t) # move from step 2 to step 3 (out of 3)
+        #now, the origin is v, and the desitination is t
 def hanoi(n,f,h,t):
+    #n: number of discs
+    #f: front postion
+    #h: helper position
+    #t: target pposition
     if n==0:
-        pass
+        pass #this is the BASE CASE. termination condition
     else:
-        hanoi(n-1,f,t,h)
+        hanoi(n-1,f,t,h) #functon call within the function definition (the recursion)!
         move(f,t)
         hanoi(n-1,h,f,t)
 hanoi(4,"A","B","C")
+
+# based on a tutorial by Professor Thorsten Altenkirch
